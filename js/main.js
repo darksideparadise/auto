4fc96d74291fa185776d78f4b825bbcf27a80191
darksideparadise <131703952+darksideparadise@users.noreply.github.com>
2023-06-29 14:54:02 +0300

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+
+    $(".rateYo").rateYo({
+        rating: 4.5,
+        starWidth: "36.228px",
+        normalFill: "#E2E6E9",
+        ratedFill: "#FFB648",
+        halfStar: true,
+        spacing: "4px"
+    });
+
+});
