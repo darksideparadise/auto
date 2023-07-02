2a27e05be9b6ece2e247023ecbd69222c44daeae
darksideparadise <131703952+darksideparadise@users.noreply.github.com>
2023-07-02 22:35:18 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,23 @@ $(function () {
         normalFill: "#E2E6E9",
         ratedFill: "#FFB648",
         halfStar: true,
-        spacing: "4px"
+        spacing: "4px",
     });
     var mixer = mixitup('.blog__list');
-});
+    $('.reviews__slider').slick({
+        arrows: false,
+        slidesToShow: 2,
+        draggable: false,
+        waitForAnimate: false,
+    })
+    $('.reviews__slider-prev').on('click', function(e) {
+        e.preventDefault()
+        $('.reviews__slider').slick('slickPrev')
+    }) 
+    $('.reviews__slider-next').on('click', function(e) {
+        e.preventDefault()
+        $('.reviews__slider').slick('slickNext')
+    })
+
+
+})   
